1fe084d5b39070b40523f50d64526edc0f411831
Cleyciane Farias <fariascleycianedelima@gmail.com>
2021-06-28 17:25:05 -0500

    CRUD em livrosControler

---
+++ Alura.WebAPI.WebApp/Api/LivrosController.cs
@@ -0,0 +1,79 @@
+﻿using Alura.ListaLeitura.Modelos;
+using Alura.ListaLeitura.Persistencia;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Alura.WebAPI.WebApp.Api
+{
+    public class LivrosController : Controller
+    {
+        private readonly IRepository<Livro> _repo;
+
+        public LivrosController(IRepository<Livro> repository)
+        {
+            _repo = repository;
+        }
+
+        [HttpGet]
+        public IActionResult Recuperar(int id)
+        {
+            var model = _repo.Find(id);
+            if (model==null)
+            {
+                return NotFound();
+            }
+            return Json(model.ToModel());
+        }
+
+        //incluir um livro a partir do seu id 
+        [HttpPost]
+        public IActionResult Incluir(LivroUpload model)
+        {
+            if (ModelState.IsValid)
+            {
+                var livro = model.ToLivro();
+                _repo.Incluir(livro);
+                var uri = Url.Action("Recuperar", new { id = livro.Id });
+                return Created(uri, livro);//create código 201
+
+            }
+            return BadRequest();
+        }
+
+
+        // fazendo a alteração dos livros que já foram inseridos
+        [HttpPost]
+        public IActionResult Alterar(LivroUpload model)
+        {
+            if (ModelState.IsValid)
+            {
+                var livro = model.ToLivro();
+                if (model.Capa == null)
+                {
+                    livro.ImagemCapa = _repo.All
+                        .Where(l => l.Id == livro.Id)
+                        .Select(l => l.ImagemCapa)
+                        .FirstOrDefault();
+                }
+                _repo.Alterar(livro);
+                return Ok(); //código 200
+            }
+            return BadRequest();
+        }
+        //método exclusão
+        [HttpPost]
+        public IActionResult Remover(int id)
+        {
+            var model = _repo.Find(id);
+            if (model == null)
+            {
+                return NotFound();
+            }
+            _repo.Excluir(model);
+            return NoContent(); //código  203
+        }
+    }
+}
